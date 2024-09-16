ce11a4d34c4825d010bc0931f7244068cedd0426
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2024-09-16 16:14:25 -0700

    Initial Commit

---
+++ boringApp/boringAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  boringAppApp.swift
+//  boringApp
+//
+//  Created by Rodrigo Ortiz on 16/09/24.
+//
+
+import SwiftUI
+
+@main
+struct boringAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
