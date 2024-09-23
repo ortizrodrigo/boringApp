87bb03db9ce07d5e8e028732a85ef06aa85ebca1
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2024-09-23 00:10:27 -0700

    added KeychainHandler

---
+++ boringApp/KeychainHandler.swift
@@ -0,0 +1,20 @@
+//
+//  KeychainHandler.swift
+//  boringApp
+//
+//  Created by Rodrigo Ortiz on 23/09/24.
+//
+
+import Foundation
+import Security
+
+struct KeychainHandler {
+    static func storePassword() {
+        
+    }
+    
+    static func retrievePassword() {
+        
+    }
+}
+
