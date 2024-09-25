0bbaeb5114fe700b5fe53b88643816a13e3f2d91
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2024-09-25 00:58:17 -0700

    Created generateSalt function to be used for user authentication

--- boringApp/KeychainHandler.swift
+++ boringApp/KeychainHandler.swift
@@ -9,11 +9,32 @@ import Foundation
 import Security
 
 struct KeychainHandler {
-    static func storePassword() {
+    
+    static func generateSalt() -> Data? {
+        let bytes = 16 // Immutable
+        var salt = Data(count: bytes) // Create Data object for salt
+        
+        // Fill the salt Data object with random bytes
+        let genStatus = salt.withUnsafeMutableBytes {
+            SecRandomCopyBytes(kSecRandomDefault, bytes, $0.baseAddress!)
+        }
+        
+        // Check the success of the random byte generation
+        if genStatus == errSecSuccess {
+            return salt
+        } else {
+            print("There was a problem generating the salt")
+            return nil
+        }
+        
+        return salt
+    }
+    
+    static func storePassword(username: String, password: String) {
         
     }
     
-    static func retrievePassword() {
+    static func retrievePassword(username: String) {
         
     }
 }
